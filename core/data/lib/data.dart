fc1d50a9c2c2cd74cf66da5663fc3a30cb60c75a
vijayakumar-h <118760167+vijayakumar-h@users.noreply.github.com>
2026-08-16 09:36:00 +0530

    feat(core): Add dio for networking Adds the `dio` package to the `core/data` module to handle network requests. The `login` feature module now includes a dependency on the `core/data` module to access the networking layer. Additionally, the root `test` script is updated to use Melos.

--- core/data/lib/data.dart
+++ core/data/lib/data.dart
@@ -1,5 +1,9 @@
-/// A Calculator.
+library;
+
+import 'package:dio/dio.dart';
+export 'package:dio/dio.dart';/// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  Dio dio = Dio();
 }
--- features/auth/login/lib/login.dart
+++ features/auth/login/lib/login.dart
@@ -1,5 +1,10 @@
+library;
+
+import 'package:data/data.dart';
+export 'package:data/data.dart';
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  Dio dio = Dio();
 }
